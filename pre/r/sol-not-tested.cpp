f1e8914c6335c9863530a846ac84d63413b5d7a9
Linas Petrauskas <linas.petrauskas@gmail.com>
2013-02-16 11:41:51 +0100

    Not tested solution to R

---
+++ pre/r/sol-not-tested.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Influence {
+    double proportion;
+    int painting_no;
+};
+
+struct Painting {
+    Painting() : greatness(0.0), solved(false) {}
+    double greatness;
+    bool solved;
+    vector<Influence> influences;
+};
+
+void Input(vector<Painting>* paintings) {
+    int N, M;
+    cin >> N, M;
+
+    paintings->resize(N + 1);  // for easier numbering
+
+    for (int i = 1; i <= N; i++) {
+        cin >> (*paintings)[i].greatness;
+    }
+
+    for (int i = 0; i < M; i++) {
+        int a;
+        Influence influence;
+        cin >> a >> influence.painting_no >> influence.proportion;
+        (*paintings)[a].influences.push_back(influence);
+    }
+}
+
+void ComputeGreatness(Painting* p, vector<Painting>* paintings) {
+    if (!p->solved) {
+        for (auto& i : p->influences) {
+            Painting& r = paintings->get(i.painting_no);
+            ComputeGreatness(&r, paintings);
+            p->greatness += i->proportion * r->greatness;
+        }
+        p->solved = true;
+    }
+}
+
+void Output(const vector<Painting>& paintings) {
+    for (int i = 1; i <= N; i++) {
+        cout << paintings[i].greatness;
+        if (i == N) {
+            cout << endl;
+        } else {
+            cout << " ";
+        }
+    }
+}
+
+int main() {
+    vector<Painting> paintings;
+    Input(&paintings);
+    
+    for (auto& p : paintings) {
+        ComputeGreatness(&p, &paintings);
+    }
+
+    Output(paintings);
+    return 0;
+}
