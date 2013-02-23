d04a08d9a9beac1d2da1c86b622a5694c5ab243e
Linas Petrauskas <linas.petrauskas@gmail.com>
2013-02-23 15:06:17 +0100

    Small fix.

--- e/sol.cpp
+++ e/sol.cpp
@@ -15,19 +15,17 @@ int main() {
   
   reverse(bytes.begin(), bytes.end());
 
-  int address = 0;
-
   bool first = true;
   for (int i = 0, pack = 0; i < bytes.size(); ++i) {
     pack = (pack << 8) | bytes[i];
     if ((i + 1) % 4 == 0 || i == bytes.size() - 1) {
       if (first) {
         first = false;
-        cout << "PUSH 0" << endl; address++;
+        cout << "PUSH 0" << endl;
       } else {
-        cout << "PUSH 1" << endl; address++;
+        cout << "PUSH 1" << endl;
       }
-      cout << "PUSH " << pack << endl; address++;
+      cout << "PUSH " << pack << endl;
     }
   }
 
@@ -40,7 +38,6 @@ int main() {
   cout << "PUSH 256" << endl;
   cout << "DIV" << endl;
   cout << "OUT" << endl;
-
   cout << "OUT" << endl;
   cout << "PUSH -12" << endl;
   cout << "JGZ" << endl;
